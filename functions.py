920816cd987af56e746fad28caf74d76312a2579
kiautumnjohnson <113151916+kiautumnjohnson@users.noreply.github.com>
2022-11-15 19:00:53 -0700

    asked user for unit

--- functions.py
+++ functions.py
@@ -6,5 +6,9 @@
 # convert the length to the correct unit 
 # output the answer to the user 
 
-user number = input ('What number to convert? ')
+user number = input ("What number to convert? ")
+user_unit = input("What unit is your number?")
+
+
 print(user_number)
+print(user_unit)
